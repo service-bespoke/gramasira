0ee0b2202cbb560994b71cde209e35800675f861
thimothy <service.bespoke@gmail.com>
2026-08-04 11:23:26 +0530

    api url updated

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost/waterbilling/index.php/api",
+  // baseURL: "http://localhost/waterbilling/index.php/api",
+  baseURL:
+    "https://gramasira.bespokewebdesignllc.com/waterbilling/index.php/api",
+
   headers: {
     "Content-Type": "application/json",
   },
